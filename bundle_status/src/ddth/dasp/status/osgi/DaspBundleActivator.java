5bfd42d94afcb58b48607a50dc0e1697f33f3e7b
Thanh Ba nguyen <btnguyen2k@gmail.com>
2012-06-19 07:53:29 +0000

    * Add service status

--- bundle_status/src/ddth/dasp/status/osgi/DaspBundleActivator.java
+++ bundle_status/src/ddth/dasp/status/osgi/DaspBundleActivator.java
@@ -1,24 +1,18 @@
 package ddth.dasp.status.osgi;
 
 import org.osgi.framework.BundleContext;
-import org.springframework.web.servlet.HandlerMapping;
 
-import ddth.dasp.common.id.IdGenerator;
 import ddth.dasp.common.osgi.BaseBundleActivator;
-import ddth.dasp.id.api.IdServiceHandlerMapping;
 
 public class DaspBundleActivator extends BaseBundleActivator {
 
-	private IdGenerator idGen = IdGenerator.getInstance(IdGenerator
-			.getMacAddr());
-	private IdServiceHandlerMapping idServiceHandlerMapping = new IdServiceHandlerMapping(idGen);
-
+	
+	
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
-		IdGenerator.disposeInstance(idGen);
 		super.stop(bundleContext);
 	}
 
@@ -27,14 +21,6 @@ public class DaspBundleActivator extends BaseBundleActivator {
 	 */
 	@Override
 	protected String getModuleName() {
-		return "ids";
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	protected HandlerMapping getSpringMvcHandlerMapping() {
-		return idServiceHandlerMapping;
+		return "status";
 	}
 }
