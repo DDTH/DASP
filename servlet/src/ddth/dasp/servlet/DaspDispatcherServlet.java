5af0581ddf4a0c60fd041a68f6cac8e1543d1e9b
Thanh Ba nguyen <btnguyen2k@gmail.com>
2012-08-02 16:58:43 +0000

    * Enable support for Tomcat Commet

--- servlet/src/ddth/dasp/servlet/DaspDispatcherServlet.java
+++ servlet/src/ddth/dasp/servlet/DaspDispatcherServlet.java
@@ -11,14 +11,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.catalina.comet.CometEvent;
+import org.apache.catalina.comet.CometProcessor;
+
 import ddth.dasp.common.DaspGlobal;
 import ddth.dasp.common.IRequestHandler;
 import ddth.dasp.common.osgi.IOsgiBootstrap;
 import ddth.dasp.common.utils.ServletUtils;
 
-public class DaspDispatcherServlet extends HttpServlet {
+public class DaspDispatcherServlet extends HttpServlet implements CometProcessor {
     private static final long serialVersionUID = 1L;
     private final static Class<IRequestHandler> HANDLER_CLASS = IRequestHandler.class;
+
+    // private Logger LOGGER =
+    // LoggerFactory.getLogger(DaspDispatcherServlet.class);
 
     private String contextPath;
     private Map<String, String> ERROR_PAGE_MAPPING = new HashMap<String, String>();
@@ -36,6 +42,26 @@ public class DaspDispatcherServlet extends HttpServlet {
         ERROR_PAGE_MAPPING.put(contextPath + ERROR_PAGE_403, ERROR_PAGE_403);
         ERROR_PAGE_MAPPING.put(contextPath + ERROR_PAGE_404, ERROR_PAGE_404);
         ERROR_PAGE_MAPPING.put(contextPath + ERROR_PAGE_500, ERROR_PAGE_500);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void event(CometEvent event) throws IOException, ServletException {
+        HttpServletRequest request = event.getHttpServletRequest();
+        HttpServletResponse response = event.getHttpServletResponse();
+        switch (event.getEventType()) {
+        case ERROR:
+        case END: {
+            event.close();
+        }
+        case BEGIN: {
+            event.setTimeout(5000);// 5 seconds
+            doHandleRequest(request, response);
+            event.close();
+        }
+        }
     }
 
     /**
