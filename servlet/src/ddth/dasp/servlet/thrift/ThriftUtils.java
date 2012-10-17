dc332f664229caaf9d91f3714a3864835cc263ef
Thanh Ba nguyen <btnguyen2k@gmail.com>
2012-10-17 11:08:46 +0000

    * Guard Thrift from OOM

--- servlet/src/ddth/dasp/servlet/thrift/ThriftUtils.java
+++ servlet/src/ddth/dasp/servlet/thrift/ThriftUtils.java
@@ -16,24 +16,30 @@ import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TServerTransport;
 import org.apache.thrift.transport.TTransportException;
 import org.apache.thrift.transport.TTransportFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import ddth.dasp.servlet.thrift.serverfactory.IServerFactory;
 
 public class ThriftUtils {
 	private final static int CLIENT_TIMEOUT = 1000;
+	private final static Logger LOGGER = LoggerFactory
+			.getLogger(ThriftUtils.class);
+	private static final long MAX_READ_BUFFER_BYTES = 1 * 1024 * 1024; // 1MB
 
 	public static TServer createThreadedServer(
 			TProcessorFactory processorFactory, int port)
 			throws TTransportException {
 		TServerTransport transport = new TServerSocket(port, CLIENT_TIMEOUT);
 		TProtocolFactory protocolFactory = new TBinaryProtocol.Factory();
-		TTransportFactory transportFactory = new TFramedTransport.Factory();
-		TThreadPoolServer server = new TThreadPoolServer(
-				new TThreadPoolServer.Args(transport)
-						.processorFactory(processorFactory)
-						.protocolFactory(protocolFactory)
-						.transportFactory(transportFactory)
-						.maxWorkerThreads(Integer.MAX_VALUE));
+		TTransportFactory transportFactory = new TFramedTransport.Factory(
+				(int) MAX_READ_BUFFER_BYTES);
+		TThreadPoolServer.Args args = new TThreadPoolServer.Args(transport)
+				.processorFactory(processorFactory)
+				.protocolFactory(protocolFactory)
+				.transportFactory(transportFactory)
+				.maxWorkerThreads(Integer.MAX_VALUE);
+		TThreadPoolServer server = new TThreadPoolServer(args);
 		return server;
 	}
 
@@ -43,12 +49,14 @@ public class ThriftUtils {
 		TNonblockingServerTransport transport = new TNonblockingServerSocket(
 				port, CLIENT_TIMEOUT);
 		TProtocolFactory protocolFactory = new TBinaryProtocol.Factory();
-		TTransportFactory transportFactory = new TFramedTransport.Factory();
-		TNonblockingServer server = new TNonblockingServer(
-				new TNonblockingServer.Args(transport)
-						.processorFactory(processorFactory)
-						.protocolFactory(protocolFactory)
-						.transportFactory(transportFactory));
+		TTransportFactory transportFactory = new TFramedTransport.Factory(
+				(int) MAX_READ_BUFFER_BYTES);
+		TNonblockingServer.Args args = new TNonblockingServer.Args(transport)
+				.processorFactory(processorFactory)
+				.protocolFactory(protocolFactory)
+				.transportFactory(transportFactory);
+		args.maxReadBufferBytes = MAX_READ_BUFFER_BYTES;
+		TNonblockingServer server = new TNonblockingServer(args);
 		return server;
 	}
 
@@ -58,11 +66,14 @@ public class ThriftUtils {
 		TNonblockingServerTransport transport = new TNonblockingServerSocket(
 				port, CLIENT_TIMEOUT);
 		TProtocolFactory protocolFactory = new TBinaryProtocol.Factory();
-		TTransportFactory transportFactory = new TFramedTransport.Factory();
-		THsHaServer server = new THsHaServer(new THsHaServer.Args(transport)
+		TTransportFactory transportFactory = new TFramedTransport.Factory(
+				(int) MAX_READ_BUFFER_BYTES);
+		THsHaServer.Args args = new THsHaServer.Args(transport)
 				.processorFactory(processorFactory)
 				.protocolFactory(protocolFactory)
-				.transportFactory(transportFactory).workerThreads(numThreads));
+				.transportFactory(transportFactory).workerThreads(numThreads);
+		args.maxReadBufferBytes = MAX_READ_BUFFER_BYTES;
+		THsHaServer server = new THsHaServer(args);
 		return server;
 	}
 
@@ -73,16 +84,17 @@ public class ThriftUtils {
 		TNonblockingServerTransport transport = new TNonblockingServerSocket(
 				port, CLIENT_TIMEOUT);
 		TProtocolFactory protocolFactory = new TBinaryProtocol.Factory();
-		TTransportFactory transportFactory = new TFramedTransport.Factory();
-		TThreadedSelectorServer server = new TThreadedSelectorServer(
-				new TThreadedSelectorServer.Args(transport)
-						.processorFactory(processorFactory)
-						.protocolFactory(protocolFactory)
-						.transportFactory(transportFactory)
-						.workerThreads(numThreads)
-						.acceptPolicy(AcceptPolicy.FAIR_ACCEPT)
-						.acceptQueueSizePerThread(10000)
-						.selectorThreads(numThreads).workerThreads(numThreads));
+		TTransportFactory transportFactory = new TFramedTransport.Factory(
+				(int) MAX_READ_BUFFER_BYTES);
+		TThreadedSelectorServer.Args args = new TThreadedSelectorServer.Args(
+				transport).processorFactory(processorFactory)
+				.protocolFactory(protocolFactory)
+				.transportFactory(transportFactory).workerThreads(numThreads)
+				.acceptPolicy(AcceptPolicy.FAIR_ACCEPT)
+				.acceptQueueSizePerThread(10000).selectorThreads(numThreads)
+				.workerThreads(numThreads);
+		args.maxReadBufferBytes = MAX_READ_BUFFER_BYTES;
+		TThreadedSelectorServer server = new TThreadedSelectorServer(args);
 		return server;
 	}
 
@@ -93,8 +105,9 @@ public class ThriftUtils {
 				try {
 					server = serverFactory.createServer();
 					server.serve();
-				} catch (Exception e) {
-					e.printStackTrace();
+				} catch (Throwable e) {
+					String msg = "Thrift server crashed: " + e.getMessage();
+					LOGGER.error(msg, e);
 					server.stop();
 					startThriftServer(serverFactory);
 				}
@@ -109,8 +122,9 @@ public class ThriftUtils {
 			public void run() {
 				try {
 					server.serve();
-				} catch (Exception e) {
-					e.printStackTrace();
+				} catch (Throwable e) {
+					String msg = "Thrift server crashed: " + e.getMessage();
+					LOGGER.error(msg, e);
 					server.stop();
 					startThriftServer(server);
 				}
