5754724e113979d33f1d4616c373c21c648d5662
btnguyen2k <btnguyen2k@gmail.com>
2013-01-04 18:09:29 +0700

    * Add Jdbc status page

---
+++ bundle_status_hetty/src/ddth/dasp/statushetty/actionhandler/JdbcStatusActionHandler.java
@@ -0,0 +1,40 @@
+package ddth.dasp.statushetty.actionhandler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import ddth.dasp.common.DaspGlobal;
+import ddth.dasp.framework.dbc.IJdbcFactory;
+
+public class JdbcStatusActionHandler extends BaseActionHandler {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected Map<String, Object> buildViewModel() {
+        Map<String, Object> model = super.buildViewModel();
+        model.put("JDBC", buildModelJdbc());
+        return model;
+    }
+
+    @SuppressWarnings("unchecked")
+    private Object buildModelJdbc() {
+        List<Object> model = new ArrayList<Object>();
+        Object temp = DaspGlobal.getGlobalVar(IJdbcFactory.GLOBAL_KEY);
+        if (!(temp instanceof Map)) {
+            temp = new HashMap<String, IJdbcFactory>();
+        }
+        Map<String, IJdbcFactory> allJdbcFactories = (Map<String, IJdbcFactory>) temp;
+        Map<String, Object> modelEntry;
+        for (Entry<String, IJdbcFactory> entry : allJdbcFactories.entrySet()) {
+            modelEntry = new HashMap<String, Object>();
+            modelEntry.put("id", entry.getKey());
+            modelEntry.put("jdbcFactory", entry.getValue());
+            model.add(modelEntry);
+        }
+        return model;
+    }
+}
