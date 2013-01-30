fc5d13d9b22d84f9e586c56978e92c53b6307c70
btnguyen2k <btnguyen2k@gmail.com>
2013-01-31 00:44:48 +0700

    * Minor fix to run on Jetty

--- servlet/src/ddth/dasp/servlet/DaspDispatcherServletTomcatCommet.java
+++ servlet/src/ddth/dasp/servlet/DaspDispatcherServletTomcatCommet.java
@@ -11,13 +11,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.catalina.comet.CometEvent;
+import org.apache.catalina.comet.CometProcessor;
+
 import ddth.dasp.common.DaspGlobal;
 import ddth.dasp.common.IRequestHandler;
 import ddth.dasp.common.api.IApiHandler;
 import ddth.dasp.common.osgi.IOsgiBootstrap;
 import ddth.dasp.common.utils.ServletUtils;
 
-public class DaspDispatcherServlet extends HttpServlet {
+public class DaspDispatcherServletTomcatCommet extends HttpServlet implements CometProcessor {
     private static final long serialVersionUID = 1L;
     private final static Class<IRequestHandler> HANDLER_CLASS = IRequestHandler.class;
 
@@ -37,6 +40,29 @@ public class DaspDispatcherServlet extends HttpServlet {
         ERROR_PAGE_MAPPING.put(contextPath + ERROR_PAGE_403, ERROR_PAGE_403);
         ERROR_PAGE_MAPPING.put(contextPath + ERROR_PAGE_404, ERROR_PAGE_404);
         ERROR_PAGE_MAPPING.put(contextPath + ERROR_PAGE_500, ERROR_PAGE_500);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void event(CometEvent event) throws IOException, ServletException {
+        HttpServletRequest request = event.getHttpServletRequest();
+        HttpServletResponse response = event.getHttpServletResponse();
+        switch (event.getEventType()) {
+        case ERROR:
+        case END: {
+            event.close();
+        }
+        case BEGIN: {
+            event.setTimeout(5000);// 5 seconds
+            doHandleRequest(request, response);
+            event.close();
+        }
+        default: {
+            break;
+        }
+        }
     }
 
     /**
