2a2f9b07a5399f62de79cb75090840df086fd77e
btnguyen2k <btnguyen2k@gmail.com>
2013-03-05 16:14:41 +0700

    * Support creating full URLs

--- bundle_framework/src/ddth/dasp/framework/url/BundleUrlCreator.java
+++ bundle_framework/src/ddth/dasp/framework/url/BundleUrlCreator.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
@@ -13,124 +14,138 @@ import ddth.dasp.common.DaspGlobal;
 
 public class BundleUrlCreator implements IUrlCreator, ServletContextAware {
 
-	private String urlSuffix;
-	private ServletContext servletContext;
-	private HttpServletResponse httpResponse;
+    private String urlSuffix;
+    private ServletContext servletContext;
+    private HttpServletResponse httpResponse;
+    private HttpServletRequest httpRequest;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public BundleUrlCreator clone() {
-		BundleUrlCreator urlCreator;
-		try {
-			urlCreator = (BundleUrlCreator) super.clone();
-		} catch (CloneNotSupportedException e) {
-			throw new RuntimeException(e);
-		}
-		urlCreator.servletContext = servletContext;
-		urlCreator.urlSuffix = urlSuffix;
-		urlCreator.httpResponse = httpResponse;
-		return urlCreator;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public BundleUrlCreator clone() {
+        BundleUrlCreator urlCreator;
+        try {
+            urlCreator = (BundleUrlCreator) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+        urlCreator.servletContext = servletContext;
+        urlCreator.urlSuffix = urlSuffix;
+        urlCreator.httpResponse = httpResponse;
+        urlCreator.httpRequest = httpRequest;
+        return urlCreator;
+    }
 
-	public String getUrlSuffix() {
-		return urlSuffix;
-	}
+    public String getUrlSuffix() {
+        return urlSuffix;
+    }
 
-	public void setUrlSuffix(String urlSuffix) {
-		this.urlSuffix = urlSuffix;
-	}
+    public void setUrlSuffix(String urlSuffix) {
+        this.urlSuffix = urlSuffix;
+    }
 
-	public HttpServletResponse getHttpResponse() {
-		return httpResponse;
-	}
+    public HttpServletResponse getHttpResponse() {
+        return httpResponse;
+    }
 
-	public void setHttpResponse(HttpServletResponse httpResponse) {
-		this.httpResponse = httpResponse;
-	}
+    public void setHttpResponse(HttpServletResponse httpResponse) {
+        this.httpResponse = httpResponse;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public String createUrl(String[] virtualParams,
-			Map<String, Object> urlParams) {
-		return createUrl(httpResponse, virtualParams, urlParams);
-	}
+    public HttpServletRequest getHttpRequest() {
+        return httpRequest;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public String createUrl(HttpServletResponse response,
-			String[] virtualParams, Map<String, Object> urlParams) {
-		return createUrl(response, virtualParams, urlParams, urlSuffix, false,
-				false);
-	}
+    public void setHttpRequest(HttpServletRequest httpRequest) {
+        this.httpRequest = httpRequest;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public String createUrl(String[] virtualParams,
-			Map<String, Object> urlParams, String urlSuffix,
-			boolean absoluteUrl, boolean forceHttps) {
-		return createUrl(httpResponse, virtualParams, urlParams, urlSuffix,
-				absoluteUrl, forceHttps);
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String createUrl(String[] virtualParams, Map<String, Object> urlParams) {
+        return createUrl(httpResponse, virtualParams, urlParams);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public String createUrl(HttpServletResponse response,
-			String[] virtualParams, Map<String, Object> urlParams,
-			String urlSuffix, boolean absoluteUrl, boolean forceHttps) {
-		ServletContext servletContext = getServletContext();
-		StringBuilder url = new StringBuilder(servletContext.getContextPath());
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String createUrl(HttpServletResponse response, String[] virtualParams,
+            Map<String, Object> urlParams) {
+        return createUrl(null, response, virtualParams, urlParams, urlSuffix, false, false,
+                DEFAULT_HTTP_PORT, DEFAULT_HTTPS_PORT);
+    }
 
-		if (virtualParams != null && virtualParams.length > 0) {
-			for (String param : virtualParams) {
-				url.append("/").append(param);
-			}
-			if (!StringUtils.isBlank(urlSuffix)
-					&& url.charAt(url.length() - 1) != '/') {
-				url.append(urlSuffix);
-			}
-		} else {
-			url.append("/");
-		}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String createUrl(String[] virtualParams, Map<String, Object> urlParams,
+            String urlSuffix, boolean absoluteUrl, boolean forceHttps) {
+        return createUrl(httpRequest, httpResponse, virtualParams, urlParams, urlSuffix,
+                absoluteUrl, forceHttps, DEFAULT_HTTP_PORT, DEFAULT_HTTPS_PORT);
+    }
 
-		if (urlParams != null && urlParams.size() > 0) {
-			url.append("?");
-			for (Entry<String, ?> entry : urlParams.entrySet()) {
-				url.append(entry.getKey());
-				url.append("=");
-				url.append(entry.getValue());
-				url.append("&");
-			}
-			url.setLength(url.length() - 1);
-		}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String createUrl(HttpServletRequest request, HttpServletResponse response,
+            String[] virtualParams, Map<String, Object> urlParams, String urlSuffix,
+            boolean absoluteUrl, boolean forceHttps, int httpPort, int httpsPort) {
+        ServletContext servletContext = getServletContext();
+        StringBuilder url = new StringBuilder(servletContext.getContextPath());
 
-		if (absoluteUrl || forceHttps) {
-			// TODO
-		}
+        for (String param : virtualParams) {
+            url.append("/").append(param);
+        }
+        if (!StringUtils.isBlank(urlSuffix)) {
+            url.append(urlSuffix);
+        }
 
-		return response != null ? response.encodeURL(url.toString()) : url
-				.toString();
-	}
+        if (urlParams != null && urlParams.size() > 0) {
+            url.append("?");
+            for (Entry<String, ?> entry : urlParams.entrySet()) {
+                url.append(entry.getKey());
+                url.append("=");
+                url.append(entry.getValue());
+                url.append("&");
+            }
+            url.setLength(url.length() - 1);
+        }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void setServletContext(ServletContext servletContext) {
-		this.servletContext = servletContext;
-	}
+        if (request != null && (absoluteUrl || forceHttps)) {
+            String domain = request.getServerName();
+            int port = request.getServerPort();
+            String scheme = request.getScheme();
+            if (!forceHttps) {
+                if (port != DEFAULT_HTTP_PORT && port != DEFAULT_HTTPS_PORT) {
+                    url.insert(0, ":" + port);
+                }
+                url.insert(0, scheme + "://" + domain);
+            } else {
+                if (httpsPort != DEFAULT_HTTPS_PORT) {
+                    url.insert(0, ":" + httpsPort);
+                }
+                url.insert(0, "https://" + domain);
+            }
+        }
 
-	protected ServletContext getServletContext() {
-		return servletContext != null ? servletContext : DaspGlobal
-				.getServletContext();
-	}
+        return response != null ? response.encodeURL(url.toString()) : url.toString();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        this.servletContext = servletContext;
+    }
+
+    protected ServletContext getServletContext() {
+        return servletContext != null ? servletContext : DaspGlobal.getServletContext();
+    }
 }
--- bundle_framework/src/ddth/dasp/framework/url/IUrlCreator.java
+++ bundle_framework/src/ddth/dasp/framework/url/IUrlCreator.java
@@ -2,9 +2,13 @@ package ddth.dasp.framework.url;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface IUrlCreator extends Cloneable {
+
+    public final static int DEFAULT_HTTP_PORT = 80;
+    public final static int DEFAULT_HTTPS_PORT = 443;
 
     /**
      * Creates a URL with default options.
@@ -42,16 +46,20 @@ public interface IUrlCreator extends Cloneable {
     /**
      * Creates a URL with full options.
      * 
+     * @param request
      * @param response
      * @param virtualParams
      * @param urlParams
      * @param urlSuffix
      * @param absoluteUrl
      * @param forceHttps
+     * @param httpPort
+     * @param httpsPort
      * @return
      */
-    public String createUrl(HttpServletResponse response, String[] virtualParams,
-            Map<String, Object> urlParams, String urlSuffix, boolean absoluteUrl, boolean forceHttps);
+    public String createUrl(HttpServletRequest request, HttpServletResponse response,
+            String[] virtualParams, Map<String, Object> urlParams, String urlSuffix,
+            boolean absoluteUrl, boolean forceHttps, int httpPort, int httpsPort);
 
     /**
      * {@inheritDoc}
