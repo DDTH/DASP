377e7d833ba88cb614e0aeea5ad403265c2ba1c7
btnguyen2k <btnguyen2k@gmail.com>
2013-03-13 11:33:46 +0700

    * global storage now uses concurrent map * move timer & scheduler back out of OSGi

--- servlet/src/ddth/dasp/common/DaspGlobal.java
+++ servlet/src/ddth/dasp/common/DaspGlobal.java
@@ -1,10 +1,8 @@
 package ddth.dasp.common;
 
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Map;
 import java.util.Timer;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -17,23 +15,17 @@ public class DaspGlobal {
 
     private static IOsgiBootstrap osgiBootstrap;
     private static TempDir contextTempDir;
-    // private static Timer contextTimer = new Timer(DaspGlobal.class.getName(),
-    // true);
-    // private static ScheduledExecutorService scheduler =
-    // Executors.newScheduledThreadPool(4);
+    private static Timer timer = new Timer(DaspGlobal.class.getName(), true);
+    private static ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(4);
     private static ServletContext servletContext;
-    private static Map<String, Object> globalStorage = new HashMap<String, Object>();
+    private static ConcurrentMap<String, Object> globalStorage = new ConcurrentHashMap<String, Object>();
 
     public static Object getGlobalVar(String name) {
-        synchronized (globalStorage) {
-            return globalStorage.get(name);
-        }
+        return globalStorage.get(name);
     }
 
     public static void setGlobalVar(String name, Object value) {
-        synchronized (globalStorage) {
-            globalStorage.put(name, value);
-        }
+        globalStorage.put(name, value);
     }
 
     protected void setOsgiBootstrap(IOsgiBootstrap osgiBootstrap) {
@@ -55,25 +47,11 @@ public class DaspGlobal {
     }
 
     public static ScheduledExecutorService getScheduler() {
-        ScheduledExecutorService scheduler = osgiBootstrap
-                .getService(ScheduledExecutorService.class);
-        if (scheduler == null) {
-            scheduler = Executors.newScheduledThreadPool(4);
-            Dictionary<String, Object> dProps = new Hashtable<String, Object>();
-            osgiBootstrap.getBundleContext().registerService(ScheduledExecutorService.class,
-                    scheduler, dProps);
-        }
         return scheduler;
     }
 
-    public static Timer getContextTimer() {
-        Timer contextTimer = osgiBootstrap.getService(Timer.class);
-        if (contextTimer == null) {
-            contextTimer = new Timer(DaspGlobal.class.getName(), true);
-            Dictionary<String, Object> dProps = new Hashtable<String, Object>();
-            osgiBootstrap.getBundleContext().registerService(Timer.class, contextTimer, dProps);
-        }
-        return contextTimer;
+    public static Timer getTimer() {
+        return timer;
     }
 
     /**
