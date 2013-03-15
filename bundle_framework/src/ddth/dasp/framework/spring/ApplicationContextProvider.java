4ed20516169435784a0a7c8dd05a767e60315ca7
btnguyen2k <btnguyen2k@gmail.com>
2013-03-15 13:24:45 +0700

    Minor fix

--- bundle_framework/src/ddth/dasp/framework/spring/ApplicationContextProvider.java
+++ bundle_framework/src/ddth/dasp/framework/spring/ApplicationContextProvider.java
@@ -1,8 +1,13 @@
 package ddth.dasp.framework.spring;
 
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.osgi.context.BundleContextAware;
+
+import ddth.dasp.common.DaspGlobal;
 
 /**
  * This class provide an application-wide access to Spring's ApplicationContext.
@@ -14,35 +19,46 @@ import org.springframework.context.ApplicationContextAware;
  * </p>
  * 
  * <pre>
- * &lt;bean id="contextApplicationContextProvider" class="...ApplicationContextProvider" /&gt;
+ * &lt;bean id="contextApplicationContextProvider" class="...ApplicationContextProvider" lazy-init="false" init-method="init" destroy-method="destroy" /&gt;
  * </pre>
  * <p>
  * In code:
  * </p>
  * 
  * <pre>
- * ApplicationContext appContext = ApplicationContextProvider.getApplicationContext();
+ * ApplicationContext appContext = ApplicationContextProvider.getApplicationContext(bundleId);
  * </pre>
  * 
  * @author NBThanh <btnguyen2k@gmail.com>
  */
-public class ApplicationContextProvider implements ApplicationContextAware {
+public class ApplicationContextProvider implements ApplicationContextAware, BundleContextAware {
 
-    private static ApplicationContext applicationContext = null;
+    private ApplicationContext applicationContext;
+    private Bundle bundle;
+    private String KEY;
+
+    public void init() {
+        DaspGlobal.setGlobalVar(KEY, applicationContext);
+    }
+
+    public void destroy() {
+        DaspGlobal.removeGlobalVar(KEY);
+    }
 
     /**
      * {@inheritDoc}
      */
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        if (ApplicationContextProvider.applicationContext == null) {
-            ApplicationContextProvider.applicationContext = applicationContext;
-        } else {
-            throw new IllegalStateException("ApplicationContext has already been set!");
-        }
+        this.applicationContext = applicationContext;
     }
 
-    public static ApplicationContext getApplicationContext() {
-        return applicationContext;
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setBundleContext(BundleContext bundleContext) {
+        this.bundle = bundleContext.getBundle();
+        KEY = "SPRING_APP_CONTEXT_" + bundle.getBundleId();
     }
 }
