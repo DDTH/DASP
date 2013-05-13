a8031453e07a8c349fa7ec24e93e070555c5c3b2
btnguyen2k <btnguyen2k@gmail.com>
2013-05-13 23:10:29 +0700

    Upgrade Spring to 3.1.4

--- test/src/ddth/dasp/test/id/TestIdGenerator.java
+++ test/src/ddth/dasp/test/id/TestIdGenerator.java
@@ -4,28 +4,70 @@ import ddth.dasp.common.id.IdGenerator;
 
 public class TestIdGenerator {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		IdGenerator idGen = IdGenerator.getInstance(IdGenerator.getMacAddr());
+    final static char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c',
+            'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+            'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 
-		System.out.println(Character.MAX_RADIX);
-		System.out.println();
+    public static String toString(long l, int radix) {
+        if (radix < Character.MIN_RADIX || radix > digits.length) {
+            radix = 10;
+        }
+        if (radix == 10) {
+            return Long.toString(l);
+        }
+        char[] buf = new char[65];
+        int charPos = 64;
+        boolean negative = (l < 0);
 
-		System.out.println(idGen.generateId48());
-		System.out.println(idGen.generateId48Hex());
-		System.out.println(idGen.generateId48Ascii());
-		System.out.println();
+        if (!negative) {
+            l = -l;
+        }
 
-		System.out.println(idGen.generateId64());
-		System.out.println(idGen.generateId64Hex());
-		System.out.println(idGen.generateId64Ascii());
-		System.out.println();
+        while (l <= -radix) {
+            buf[charPos--] = digits[(int) (-(l % radix))];
+            l = l / radix;
+        }
+        buf[charPos] = digits[(int) (-l)];
 
-		System.out.println(idGen.generateId128());
-		System.out.println(idGen.generateId128Hex());
-		System.out.println(idGen.generateId128Ascii());
-		System.out.println();
-	}
+        if (negative) {
+            buf[--charPos] = '-';
+        }
+
+        return new String(buf, charPos, (65 - charPos));
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        long l = 12345;
+        System.out.println(toString(l, 46));
+
+        IdGenerator idGen = IdGenerator.getInstance(IdGenerator.getMacAddr());
+
+        System.out.println(Character.MAX_RADIX);
+        System.out.println();
+
+        long value;
+
+        value = idGen.generateId48();
+        System.out.println(idGen.generateId48());
+        System.out.println(idGen.generateId48Hex());
+        System.out.println(idGen.generateId48Ascii());
+        System.out.println(toString(value, 46));
+        System.out.println();
+
+        value = idGen.generateId64();
+        System.out.println(idGen.generateId64());
+        System.out.println(idGen.generateId64Hex());
+        System.out.println(idGen.generateId64Ascii());
+        System.out.println(toString(value, 46));
+        System.out.println();
+
+        System.out.println(idGen.generateId128());
+        System.out.println(idGen.generateId128Hex());
+        System.out.println(idGen.generateId128Ascii());
+        System.out.println();
+    }
 }
